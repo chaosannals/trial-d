048d70640f480fe27a7f379417f6178d6be16bcb
chaosannals <chaosannals@outlook.com>
2021-11-10 21:05:53 +0800

    cmdargs

--- jrexp/source/app.d
+++ jrexp/source/app.d
@@ -1,4 +1,5 @@
 import std.stdio;
+import std.algorithm;
 import std.string;
 import std.path;
 import std.array;
@@ -8,17 +9,22 @@ import onyx.bundle;
 
 void main(string[] args)
 {
+	// 初始化日志
 	auto bundle = new immutable Bundle("./conf/log.conf");
 	createLoggers(bundle);
-
 	auto log = getLogger("DebugLogger");
+
+	// 应用配置
+	auto appBundle = new immutable Bundle("./conf/app.conf");
+	auto command = array(appBundle.values("App", "command").map!(i => i.strip("\"")));
+	log.debug_("cps: %s", command);
 
 	auto jrePath = absolutePath("jre");
 	auto jreBinPath = buildPath(jrePath, "bin");
 	auto envPath = join([jreBinPath, environment["PATH"]], ";");
 	environment["PATH"] = envPath;
 	log.info("jre: %s",  jrePath);
-	auto pipes = pipeProcess(["javaw", "-version"]);
+	auto pipes = pipeProcess(command);
 	foreach (line; pipes.stdout.byLine) {
 		log.info("jre: %s", strip(line.idup));
 	}
