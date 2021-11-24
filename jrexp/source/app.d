a77e3d0d62984b67012e50a97f242427d9a835a0
chaosannals <chaosannals@outlook.com>
2021-11-24 09:40:53 +0800

    变量替换。

--- jrexp/source/app.d
+++ jrexp/source/app.d
@@ -18,7 +18,7 @@ private string[] LOG_CONF_DEFAULT = [
 ];
 private string[] APP_CONF_DEFAULT = [
 	"[App]",
-	"command = javaw -cp \".;./*\" -Djava.library.path=./bin exert.d.jrejar.App",
+	"command = ${jredir}/bin/javaw -cp \".;./*\" -Djava.library.path=./bin exert.d.jrejar.App",
 ];
 
 private immutable(Bundle) loadBundle(string path, string[] ds)
@@ -30,23 +30,7 @@ private immutable(Bundle) loadBundle(string path, string[] ds)
 	return new immutable Bundle(ds);
 }
 
-void main(string[] args)
-{
-	// 初始化日志
-	auto bundle = loadBundle(LOG_CONF_PATH, LOG_CONF_DEFAULT);
-	createLoggers(bundle);
-	auto log = getLogger("DebugLogger");
-
-	// 应用配置
-	auto appBundle = loadBundle(APP_CONF_PATH, APP_CONF_DEFAULT);
-	auto command = array(appBundle.values("App", "command").map!(i => i.strip("\"")));
-	log.debug_("cps: %s", command);
-
-	auto jrePath = absolutePath("jre");
-	auto jreBinPath = buildPath(jrePath, "bin");
-	const envPath = join([jreBinPath, environment["PATH"]], ";");
-	environment["PATH"] = envPath;
-	log.info("jre: %s", jrePath);
+private void run(Logger log, string[] command) {
 	auto pipes = pipeProcess(command);
 	foreach (line; pipes.stdout.byLine)
 	{
@@ -57,4 +41,26 @@ void main(string[] args)
 		log.error("jre: %s", strip(line.idup));
 	}
 	wait(pipes.pid);
+}
+
+void main(string[] args)
+{
+	// 路径
+	auto jrePath = absolutePath("jre");
+	auto jreBinPath = buildPath(jrePath, "bin");
+
+	// 初始化日志
+	auto bundle = loadBundle(LOG_CONF_PATH, LOG_CONF_DEFAULT);
+	createLoggers(bundle);
+	auto log = getLogger("DebugLogger");
+
+	// 应用配置
+	auto appBundle = loadBundle(APP_CONF_PATH, APP_CONF_DEFAULT);
+	auto command = array(appBundle.values("App", "command").map!(i => i.strip("\"").replace("${jredir}", jrePath)));
+	log.debug_("cps: %s", command);
+
+	const envPath = join([jreBinPath, environment["PATH"]], ";");
+	environment["PATH"] = envPath;
+	log.info("jre: %s", jrePath);
+	run(log, command);
 }
